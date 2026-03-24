09294b4bbd038a95c39a43dd912e273481093a44
shashank-386045 <shashank.nuthalapati06@gmail.com>
2026-03-24 08:45:53 +0530

    UC2: Initialize Train and Display Consist Summary

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,11 +1,25 @@
 import java.util.ArrayList;
 import java.util.List;
-public class TrainConsistManagementApp{
+
+public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
-        System.out.println("=== Train Consist Management App ===");
-        List<String> trainConsist = new ArrayList<>();
-        System.out.println("Initial bogie count: " + trainConsist.size());
-        System.out.println("Train consist initialized successfully.");
+        System.out.println("=== Train Consist Management App (UC2) ===");
+
+        List<String> passengerBogies = new ArrayList<>();
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("Bogies after insertion: " + passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+        System.out.println("Bogies after removing AC Chair: " + passengerBogies);
+
+        boolean hasSleeper = passengerBogies.contains("Sleeper");
+        System.out.println("Does Sleeper bogie exist? " + hasSleeper);
+
+        System.out.println("Final bogie list: " + passengerBogies);
     }
 }
