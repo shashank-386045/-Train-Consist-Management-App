1dee326da31ccd6da8eae563b27fa9639fb3b0a6
shashank-386045 <shashank.nuthalapati06@gmail.com>
2026-04-07 21:09:20 +0530

    Base application setup

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,28 @@
+import java.util.*;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+}
+
 public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 48));
+
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+
+        System.out.println("Sorted Bogies by Capacity:");
+        for (Bogie b : bogies) {
+            System.out.println(b.name + " - " + b.capacity);
+        }
+    }
 }
